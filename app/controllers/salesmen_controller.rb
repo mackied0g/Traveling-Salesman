65304cf4ed1e7d719d9d0d1189cd5e8a8654c10b
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-08-19 12:07:35 -0400

    Updated controllers

--- app/controllers/salesmen_controller.rb
+++ app/controllers/salesmen_controller.rb
@@ -1,2 +1,47 @@
 class SalesmenController < ApplicationController
-end
+
+    before_action :find_salesman, only: [:show, :edit, :update, :destroy]
+        
+        
+        def index
+            @salesmen = Salesman.all
+        end
+        
+        def show
+        
+        end
+        
+        def new
+            @saleman = Salesman.new
+        end
+        
+        def create
+            @salesman = Salesman.create(salesman_params)
+            redirect_to @salesman 
+        end
+        
+        def edit
+        end
+        
+        def update
+            @salesman.update(salesman_params)
+            redirect_to @salesman
+        end
+        
+        
+        def destroy
+            @salesman.destroy
+            redirect_to salesmen_path
+        end
+        
+        private
+        
+        def find_salesman
+            @salesman = salesman.find(params[:id])
+        end
+        
+        def salesman_params
+            params.require(:salesman).permit(:name, :company)
+        end
+    
+    end

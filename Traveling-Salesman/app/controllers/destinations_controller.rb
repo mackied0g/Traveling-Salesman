1aae0f29682638d3d9cf44ec4ef07eb033e49fda
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-08-19 12:09:03 -0400

    Updated controllers

--- Traveling-Salesman/app/controllers/destinations_controller.rb
+++ Traveling-Salesman/app/controllers/destinations_controller.rb
@@ -1,2 +1,49 @@
-class DestinationsController < ApplicationController
-end
+class destinationsController < ApplicationController
+
+    before_action :find_destination, only: [:show, :edit, :update, :destroy]
+        
+        
+        def index
+            @destinations = Destination.all
+        end
+        
+        def show
+        
+        end
+        
+        def new
+            @destination = Destination.new
+        end
+        
+        def create
+            @destination = Destination.create(destination_params)
+            redirect_to @destination 
+        end
+        
+        def edit
+        end
+        
+        def update
+            @destination.update(destination_params)
+            redirect_to @destination
+        end
+        
+        
+        def destroy
+            @destination.destroy
+            redirect_to destinations_path
+        end
+
+        
+        
+        private
+        
+        def find_destination
+            @destination = Destination.find(params[:id])
+        end
+        
+        def destination_params
+            params.require(:destination).permit(:location, :about_info)
+        end
+    
+    end
--- Traveling-Salesman/app/controllers/flights_controller.rb
+++ Traveling-Salesman/app/controllers/flights_controller.rb
@@ -1,2 +1,49 @@
 class FlightsController < ApplicationController
-end
+
+    before_action :find_flight, only: [:show, :edit, :update, :destroy]
+        
+        
+        def index
+            @flights = Flight.all
+        end
+        
+        def show
+        
+        end
+        
+        def new
+            @flight = Flight.new
+        end
+        
+        def create
+            @flight = Flight.create(flight_params)
+            redirect_to @flight 
+        end
+        
+        def edit
+        end
+        
+        def update
+            @flight.update(flight_params)
+            redirect_to @flight
+        end
+        
+        
+        def destroy
+            @flight.destroy
+            redirect_to flights_path
+        end
+
+        
+        
+        private
+        
+        def find_flight
+            @flight = Flight.find(params[:id])
+        end
+        
+        def flight_params
+            params.require(:flight).permit(:destination_id, :star_rating, :guest_rating, :is_available?)
+        end
+    
+    end

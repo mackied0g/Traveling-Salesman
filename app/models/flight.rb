ac2781375eea64173a0c34232985f4bd069f7bfa
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-08-19 15:46:50 -0400

    update views

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,7 @@
 class Flight < ApplicationRecord
+    belongs_to :destination
+    has_many :itineraries
+    has_many :hotels, through: :itineraries
+    has_many :salesmen, through: :itineraries
+
 end

de18b117d268275f01b2f4c213df076471b92959
jason <jasonedlewis@gmail.com>
2019-08-20 14:12:54 -0400

    added login model, made controller and routes

---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,11 @@
+class LoginsController < ApplicationController
+
+    def new
+        
+    end
+
+    def create 
+    
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :logins
   resources :flights
   resources :hotels
   resources :destinations
   resources :itineraries
   resources :salesmen
+  get '/login', to: 'login#new'
+  post '/login', to: 'login#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

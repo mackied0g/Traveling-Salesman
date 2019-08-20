295f4d83a8a2506c85c5efe81b47d3ff11b1e98c
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-08-20 19:14:58 -0400

    updated application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
+
 class ApplicationController < ActionController::Base
-end
+    before_action :set_up_auth_variables
+  
+    def set_up_auth_variables
+  
+      @salesman_id = session["salesman_id"]
+      @logged_in = !!@salesman_id
+      if @logged_in
+        @current_salesman = salesman.find(@salesman_id)
+        @can_book_itinerary = @current_salesman.can_book_itinerary
+      end
+  
+    end
+  
+    def authorized
+      unless @logged_in
+        return redirect_to salesman_path
+      end
+    end
+  
+    def logout
+      session["salesman_id"] = nil
+    end

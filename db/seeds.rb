043058c8a671bd316156e25923b8b6d81bbcf189
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-08-20 08:18:15 -0400

    created salesmen/index, updated salesmen/show, form and added a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ron = Salesman.create(name: "Ron Howard", company: "P&G", email: "helloron@ronhoward.com", password: "1d&3G:7lR%?")

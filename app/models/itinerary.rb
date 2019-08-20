8fd39dfb6a0b00f9fedc3937f5b638cfd20f4cf7
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-08-20 08:01:00 -0400

    changed "saleman" to "salesman"

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,7 +1,11 @@
 class Itinerary < ApplicationRecord
-    belongs_to :saleman
+    belongs_to :salesman
     belongs_to :flight
     belongs_to :hotel
     belongs_to :destination
 
+    validates :salesman, presence: true
+    validates :destination, presence: true
+    validates :flight, presence: true
+    validates :hotel, presence: true
 end

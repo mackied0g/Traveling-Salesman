e18fe7370c2b851673e65034ea57f3a8dc218865
jason <jasonedlewis@gmail.com>
2019-08-21 10:17:58 -0400

    got all pages working and added home root for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  
+  get 'home/index'
   resources :flights
   resources :hotels
   resources :destinations
@@ -8,5 +9,6 @@ Rails.application.routes.draw do
   get '/login', to: 'login#new'
   post '/login', to: 'login#create'
   delete '/logout', to: 'login#destory'
+  root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

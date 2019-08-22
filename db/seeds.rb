6f91a263e1628677e7151a3d4a62f8217eb2ddec
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-08-22 11:51:01 -0400

    got seeds.rb to compile!!!!!!!!!!!!!!!!!

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,14 +18,14 @@ nyc = Destination.create(location: "Manhattan", about_info: "Ay, I'm walkin' her
 
 # tc_or_local, year, month = 1, day = 1, hour = 0, min = 0, sec = 0)
 
-delta123 = Flight.create(destination_id: destination.id, flight_date: (2020, 05, 11, 16, 13, 00), price: 420.42, class: "business", flight_number: "Delta123")
-sw123 = Flight.create(destination_id: destination.id, flight_date: 2008-05-11 16:40:00, price: 890.42, class: "first", flight_number: "SouthWest123")
-can123 = Flight.create(destination_id: destination.id, flight_date: 2010-05-11 16:40:00, price: 40.42, class: "business", flight_number: "CanadaAir123")
-united123 = Flight.create(destination_id: destination.id, flight_date: 1990-05-11 16:40:00, price: 310.42, class: "first", flight_number: "United123")
+#delta123 = Flight.create(destination_id: destination.id, flight_date: (%d/%m/%y: %H:%M:%S %Z), price: 420.42, level: "business", flight_number: "Delta123")
+sw123 = Flight.create(destination_id: aruba.id, flight_date: (03/12/2008), price: 890.42, level: "first", flight_number: "SouthWest123")
+can123 = Flight.create(destination_id: equador.id, flight_date: (2010-05-11), price: 40.42, level: "business", flight_number: "CanadaAir123")
+united123 = Flight.create(destination_id: nyc.id, flight_date: (1990-05-11), price: 310.42, level: "first", flight_number: "United123")
 
 
-hiltonparis = Hotel.create(destination_id: destination.id, star_rating: 4, guest_rating: 9, is_available?: true, name: "The Hilton Paris")
-ramadaflushing = Hotel.create(destination_id: destination.id, star_rating: 2, guest_rating: 5, is_available?: false, name: "The Ramada Flushing")
-daysinn = Hotel.create(destination_id: destination.id, star_rating: 1, guest_rating: 3, is_available?: true, name: "Days Inn")
-mariottmarquis = Hotel.create(destination_id: destination.id, star_rating: 4, guest_rating: 10, is_available?: true, name: "The Mariott Marquis Times Square")
+hiltonparis = Hotel.create(destination_id: aruba.id, star_rating: 4, guest_rating: 9, is_available?: true, name: "The Hilton Paris")
+ramadaflushing = Hotel.create(destination_id: aruba.id, star_rating: 2, guest_rating: 5, is_available?: false, name: "The Ramada Flushing")
+daysinn = Hotel.create(destination_id: aruba.id, star_rating: 1, guest_rating: 3, is_available?: true, name: "Days Inn")
+mariottmarquis = Hotel.create(destination_id: equador.id, star_rating: 4, guest_rating: 10, is_available?: true, name: "The Mariott Marquis Times Square")
 
